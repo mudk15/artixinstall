64878e2284aa3c7b5d5fb9dd534c1855e9e63aef
mudk15 <86729201+mudk15@users.noreply.github.com>
2021-10-11 22:22:11 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -8,8 +8,15 @@ echo print hostname
 read hostname
 echo print console font (cyr-sun16 for russian)
 read consolfont
+echo print locale (for example ru_RU)
+read local
 # пошло говно по трубам
 basestrap /mnt base base-devel $initsys elogind-$initsys networkmanager-$initsys $kernel $kernel-headers linux-firmware grub os-prober efibootmgr sudo nano
 echo $hostname > /mnt/etc/hostname
 echo 127.0.1.1 localhost.localdomain $hostname > /mnt/etc/hosts
 echo $consolfont > /mnt/etc/vconsole.conf
+echo LANG="$local.UTF-8" >> /mnt/etc/locale.conf
+echo LC_COLLATE="C" >> /mnt/etc/locale.conf
+echo $local.UTF-8 UTF-8 >> /mnt/etc/locale.gen
+echo $en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
+artix-chroot /mnt
