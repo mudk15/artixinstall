9651bd0863fde9a931de6d6e6d5b692d0affe302
mudk15 <86729201+mudk15@users.noreply.github.com>
2021-10-12 16:31:39 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-echo print init system (runit, openrc only for now)
-read initsys
-echo print kernel (linux, linux-zen, linux-lts)
-read kernel
+stad=1
+case $stad in
+1)
+echo "print init system (runit and openrc only for now)"
+read -p "Enter: " initsys
+echo "print kernel (linux, linux-zen, linux-lts)"
+read -p "Enter: " kernel
 echo print hostname
-read hostname
-echo print console font (cyr-sun16 for russian)
-read consolfont
-echo print locale (for example ru_RU)
-read local
-# пошло говно по трубам
-basestrap /mnt base base-devel $initsys elogind-$initsys networkmanager-$initsys $kernel $kernel-headers linux-firmware grub os-prober efibootmgr sudo nano
-echo $hostname > /mnt/etc/hostname
-echo 127.0.1.1 localhost.localdomain $hostname > /mnt/etc/hosts
-echo $consolfont > /mnt/etc/vconsole.conf
-echo LANG="$local.UTF-8" >> /mnt/etc/locale.conf
-echo LC_COLLATE="C" >> /mnt/etc/locale.conf
-echo $local.UTF-8 UTF-8 >> /mnt/etc/locale.gen
-echo $en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
-artix-chroot /mnt
+read -p "Enter: " hostname
+echo "print console font (cyr-sun16 for russian)"
+read -p "Enter: " consolfont
+echo "print locale (for example ru_RU)"
+read -p "Enter: " local
+stad=2
+;;
+2)
+echo "Are you sure?
+no - return to start
+yes - contunue
+exit - exit with no changes"
+read answer
+case $answer in
+no)
+stad=1
+;;
+"exit")
+exit
+;;
+*)
+stad=2
+;;
+esac
+;;
+esac
