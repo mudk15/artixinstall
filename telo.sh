be664e8379f82fe7491731d632356eeb8d29f38f
mudk15 <86729201+mudk15@users.noreply.github.com>
2021-10-12 20:53:06 +0300

    Update telo.sh

--- telo.sh
+++ telo.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "ln -sf /usr/share/zoneinfo/$tmzn /etc/localtime
+echo "ln -sf /usr/share/zoneinfo/\$tmzn /etc/localtime
 hwclock --systohc
 locale-gen
 echo your system is 1 - BIOS, 2 - EFI?
 read syst
-case $syst in
+case \$syst in
 1)
 echo choice disk for bios
 lsblk
 read diskbios
-grub-install --recheck $diskbios
+grub-install --recheck \$diskbios
 grub-mkconfig -o /boot/grub/grub.cfg
 ;;
 2)
 echo print bootloader id
 read bootid
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$bootid
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=\$bootid
 grub-mkconfig -o /boot/grub/grub.cfg
 ;;
 esac
@@ -23,11 +23,11 @@ echo print root password
 passwd
 echo print username
 read usrnm
-useradd -m -g users -G wheel -s /bin/bash $usrnm
+useradd -m -g users -G wheel -s /bin/bash \$usrnm
 echo %wheel ALL=(ALL) ALL >> /etc/sudoers
-echo print pass for $usrnm
-passwd $usrnm
-case $initsys in
+echo print pass for \$usrnm
+passwd \$usrnm
+case \$initsys in
 runit)
 ln -s /etc/runit/sv/NetworkManager/ /etc/runit/runsvdir/default
 ;;
