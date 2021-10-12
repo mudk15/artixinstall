8ca357e983f1392cd909a531de9b0e8d7e9a8554
mudk15 <86729201+mudk15@users.noreply.github.com>
2021-10-12 16:57:05 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-stad=1
-case $stad in
-1)
 echo "print init system (runit and openrc only for now)"
 read -p "Enter: " initsys
 echo "print kernel (linux, linux-zen, linux-lts)"
@@ -12,24 +9,31 @@ echo "print console font (cyr-sun16 for russian)"
 read -p "Enter: " consolfont
 echo "print locale (for example ru_RU)"
 read -p "Enter: " local
-stad=2
-;;
-2)
 echo "Are you sure?
-no - return to start
 yes - contunue
-exit - exit with no changes"
+any - exit with no changes"
 read answer
 case $answer in
-no)
-stad=1
-;;
-"exit")
-exit
+yes)
+# пошло говно по трубам (yes)
+basestrap /mnt base base-devel $initsys elogind-$initsys networkmanager-$initsys $kernel $kernel-headers linux-firmware grub os-prober efibootmgr sudo nano
+echo $hostname > /mnt/etc/hostname
+echo "127.0.1.1 localhost.localdomain $hostname" > /mnt/etc/hosts
+echo FONT=$consolfont > /mnt/etc/vconsole.conf
+echo LANG="$local.UTF-8" >> /mnt/etc/locale.conf
+echo LC_COLLATE="C" >> /mnt/etc/locale.conf
+  # язык
+if [ $local != en-US ]
+then
+echo $local.UTF-8 UTF-8 >> /mnt/etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /mnt/etc/locale.gen
+else
+echo $local.UTF-8 UTF-8 >> /mnt/etc/locale.gen
+fi
+artix-chroot /mnt
 ;;
 *)
-stad=2
-;;
-esac
+# любой другой ответ
+exit
 ;;
 esac
