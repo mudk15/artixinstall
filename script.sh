f6b5eeea3e9ca941ec819d09e3997e433153d2eb
Your Name <you@example.com>
2022-01-08 21:43:15 +0300

    script

--- script.sh
+++ script.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 PS3="  Print number: "
 echo -e "\n\tSelect init"
-select init in runit openrc;do echo -e "\n\tYou select $init\n";break;done
+select init in runit openrc;do 
+echo -e "\n\tYou select $init\n";break;done
 
 echo -e "\n\tSelect kernel"
-select kernel in linux linux-zen linux-lts linux-hardened;do echo -e "\n\tYou select $kernel\n";break;done
+select kernel in linux linux-zen linux-lts linux-hardened;do 
+echo -e "\n\tYou select $kernel\n";break;done
 
 echo -e "\n\tSelect ucode"
-select ucode in "amd-ucode" "intel-ucode";do echo -e "\n\tYou select $ucode\n";break;done
+select ucode in "amd-ucode" "intel-ucode";do 
+echo -e "\n\tYou select $ucode\n";break;done
 
 echo -e "\n\tSelect editor"
-select texted in nano vim;do echo -e "\n\tYou select $texted\n";break;done
+select texted in nano vim;do 
+echo -e "\n\tYou select $texted\n";break;done
 
 echo -e "\n\tSelect dhcp client"
-select dhcpclient in dhcpcd dhclient;do echo -e "\n\tYou select $dhcpclient\n";break;done
+select dhcpclient in dhcpcd dhclient;do 
+echo -e "\n\tYou select $dhcpclient\n";break;done
 
 if [[ -z $userlogin ]];then
 echo -e "\tPrint user login"
@@ -23,7 +28,7 @@ fi
 
 if [[ -z $hostname ]];then
 echo -e "\n\tPrint hostname"
-until [[ $hostname =~ [a-zA-Z0-9-.]+ ]];do
+until [[ $hostname =~ ^[a-zA-Z][0-9\-\.]+ ]];do
 read -rp "  Print hostname: " hostname;done
 fi
 
