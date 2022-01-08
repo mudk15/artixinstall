e3a7ba7cc455bf9cdf0f01392bdbd9f04db35f9d
Your Name <you@example.com>
2022-01-08 23:41:08 +0300

    script

--- script.sh
+++ script.sh
@@ -21,6 +21,10 @@ echo -e "\n\tSelect dhcp client"
 select dhcpclient in dhcpcd dhclient;do 
 echo -e "\n\tYou select $dhcpclient\n";break;done
 
+echo -e "\n\tSelect network manager"
+select netmgr in connman networkmanager;do 
+echo -e "\n\tYou select $netmgr\n";break;done
+
 if [[ -z $userlogin ]];then
 echo -e "\tPrint user login"
 until [[ $userlogin =~ ^([a-z]+)$ ]];do
@@ -39,14 +43,23 @@ until [[ $timezone =~ ^([A-Z]{1})([a-z]+/)([A-Z]{1})([a-z]+)$ ]];do
 read -rp "  Print timezone: " timezone;done
 fi
 
+if [[ -z $locale ]];then
+echo -e "\tPrint locale"
+until [[ $userlogin =~ ^([a-z]+_)([A-Z]+)$ ]];do
+read -rp "  Print username: " userlogin;done
+fi
+
 echo -e "
 \tInit\t\t - $init
 \tKernel\t\t - $kernel
 \tUcode\t\t - $ucode
 \tEditor\t\t - $texted
 \tDHCP\t\t - $dhcpclient
+\tNetwork Manager\t - $netmgr
 \tUser Login\t - $userlogin
 \tHostname\t - $hostname
+\tTimezone\t - $timezone
+\tlocale\t\t - $locale
 "
 
 #basestrap /mnt/ base base-devel $init elogind-$init $kernel $ucode linux-firmware $texted sudo grub os-prober efibootmgr $dhcpclient
