ed99692a9877aff7863821fbc642e0c69f6042ec
Your Name <you@example.com>
2022-01-08 21:58:12 +0300

    script

--- script.sh
+++ script.sh
@@ -22,13 +22,13 @@ echo -e "\n\tYou select $dhcpclient\n";break;done
 
 if [[ -z $userlogin ]];then
 echo -e "\tPrint user login"
-until [[ $userlogin =~ [a-z]+ ]];do
+until [[ $userlogin =~ ^([a-z]+)& ]];do
 read -rp "  Print username: " userlogin;done
 fi
 
 if [[ -z $hostname ]];then
 echo -e "\n\tPrint hostname"
-until [[ $hostname =~ ^[a-zA-Z][0-9\-\.]+ ]];do
+until [[ $hostname =~ ^([a-zA-Z]+)[a-zA-Z0-9\-\.]([a-zA-Z0-9]+)$ ]];do
 read -rp "  Print hostname: " hostname;done
 fi
 
