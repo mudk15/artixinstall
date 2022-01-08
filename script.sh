f876d67f5d76053847a5d1ff558e99a003242c7d
Your Name <you@example.com>
2022-01-08 20:59:22 +0300

    script

--- script.sh
+++ script.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 PS3="  Print number: "
-echo -e "\n\tSelect init\n"
+echo -e "\n\tSelect init"
 select init in runit openrc;do echo -e "\n\tYou select $init\n";break;done
 
-echo -e "\n\tSelect kernel\n"
+echo -e "\n\tSelect kernel"
 select kernel in linux linux-zen linux-lts linux-hardened;do echo -e "\n\tYou select $kernel\n";break;done
 
-echo -e "\n\tSelect ucode\n"
+echo -e "\n\tSelect ucode"
 select ucode in "amd-ucode" "intel-ucode";do echo -e "\n\tYou select $ucode\n";break;done
 
-echo -e "\n\tSelect editor\n"
+echo -e "\n\tSelect editor"
 select texted in nano vim;do echo -e "\n\tYou select $texted\n";break;done
 
-echo -e "\n\tSelect dhcp client\n"
+echo -e "\n\tSelect dhcp client"
 select dhcpclient in dhcpcd dhclient;do echo -e "\n\tYou select $dhcpclient\n";break;done
 
 if [[ -z $userlogin ]];then
-echo -e "\tUsername is empty"
+echo -e "\tPrint user login"
 until [[ $userlogin =~ [a-z]+ ]];do
 read -rp "  Print username: " userlogin
 done
 fi
 
 if [[ -z $hostname ]];then
+echo -e "\n\tPrint hostname"
 until [[ $hostname =~ [a-zA-Z0-9\-\.]+ ]];do
 read -rp "  Print hostname: " hostname
 done
